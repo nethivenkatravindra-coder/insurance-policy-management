ac9bcb09862808dcb4ef2a14c98c7e484143c5c5
nethivenkatravindra-coder <nethivenkatravindra@gmail.com>
2026-07-05 16:46:47 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+const buttons=document.querySelectorAll("button");
+
+buttons.forEach(btn=>{
+
+btn.addEventListener("click",()=>{
+
+alert("Insurance Quote Request Submitted Successfully!");
+
+});
+
+});
